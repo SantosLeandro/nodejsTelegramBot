f18dbaa8ef6b91978fda0bdcc8809fb1d935a7a1
leandro <santos.leandrom@outlook.com>
2022-01-09 18:08:30 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const https = require('https');
+const PORT = process.env.PORT || 5000;
+const token = process.env.TOKEN;
+
+function sendMessage(chatId, text){
+    https.get('https://api.telegram.org/bot'+token+'/sendMessage?chatId='+chatId+'&text='+text,()=>{
+
+    });
+}
+
+app.use('',(req, res)=>{
+    console.log(req.body);
+    sendMessage(req.body.message.id,'Hello!');
+    res.sendStatus( 200 );
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
