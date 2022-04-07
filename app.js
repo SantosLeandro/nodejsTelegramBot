cb76b54b5272c64307c71c31441ca4de9b0de085
SantosLeandro <31940650+SantosLeandro@users.noreply.github.com>
2022-04-07 12:23:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const https = require('https');
 const PORT = process.env.PORT || 5000;
 const token = process.env.TOKEN;
+const botName = process.env.BOTNAME;
 
 function sendMessage(chatId, text){
     https.get('https://api.telegram.org/bot'+token+'/sendMessage?chatId='+chatId+'&text='+text,()=>{
@@ -10,9 +11,20 @@ function sendMessage(chatId, text){
     });
 }
 
+
+function checkCommand(message, command, callback){
+	if(message.text == command || message.text == (command+"@"+botName)){
+		callback();
+	}
+}
+
 app.use('',(req, res)=>{
     console.log(req.body);
-    sendMessage(req.body.message.id,'Hello!');
+	var chatId = req.body.message.chat.id;
+	var message = req.body.message.text;
+	checkCommand(message, "/salve", ()=>sendMessage(chatId, "Hello!"));
+	//if(checkCommand(message,"/salve"))
+	//	sendMessage(chatId,'Hello!');
     res.sendStatus( 200 );
 });
 
